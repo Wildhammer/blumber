67e7c7b27fd6eefe57bf3df9c1439ff45fe03022
Mohammed Behbooei <behbooeid@yahoo.com>
2017-01-17 21:37:42 -0500

    successfully writes to the file.

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from scp import SCPClient
 import json
 from pprint import pprint
 import sys
+import datetime
+import calendar
+import random
 
-if len(sys.argv) < 6 :
-	print "username password hostname port key_path"
+if len(sys.argv) < 2 :
+	print "ssh credentials are needed"
 	sys.exit()
 
-uname = sys.argv[1]
-pswrd = sys.argv[2]
-hst = sys.argv[3]
-prt = int(sys.argv[4])
-key_path = sys.argv[5]
+with open(sys.argv[1]) as data_file:    
+    credentials = json.load(data_file)    
+
+uname = credentials['username']
+pswrd = credentials['password']
+hst = credentials['hostname']
+prt = int(credentials['port'])
+key_path = credentials['key_path']
 
 
 k = paramiko.dsskey.DSSKey(filename=key_path,password=pswrd)
@@ -31,8 +37,30 @@ scp.get('public_html/posts.json','./')
 with open('posts.json') as data_file:
     data = json.load(data_file)
 
-data.append('post'+str(len(data)+1)+'.htm')
+next_post_num = str(len(data)+1)
+data.append('post'+next_post_num+'.htm')
 
+file = open("post"+next_post_num+".htm", "w")
+
+now = datetime.datetime.now()
+r = lambda: random.randint(0,255)
+
+title = raw_input("What's this post about?\n")
+file.write("<hr>\n")		
+file.write("<h2>"+title+"</h2>\n")
+file.write("<h5><span class=\"glyphicon glyphicon-time\"></span> Post by "+calendar.month_name[now.month]+" "+str(now.day)+", "+str(now.year)+".</h5>")
+tags = raw_input("What kind of tags do you think this post is related to?\nSeprate tags with \',\' character.\n")
+array = filter(None, tags.replace(",", " ").split(' '))
+file.write("<h5>")
+for x in array:
+	file.write("<span class=\"label\" style=\"background-color:"+"#%02X%02X%02X" % (r(),r(),r()) +"\">"+x+"</span>")
+file.write("</h5><br>\n")	
+body = raw_input("Now type in the body of your post:\n(Remember this is plain html so feel free to use any phrase element you want.\ne.g. <pre> for code snippet, <code> for a command, <br> for new line etc.)\n")
+file.write("<p>")
+file.write(body)
+file.write("</p>")
+
+file.close()
 
 
 scp.close()
